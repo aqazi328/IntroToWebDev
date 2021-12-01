d2b529724392b3871c29e7a1924c946ceeb801d0
Ahson Qazi <70555455+aqazi328@users.noreply.github.com>
2021-12-01 14:01:36 -0600

    moving files over

---
+++ portfolio/rollingDice.js
@@ -0,0 +1,10 @@
+var numSides = prompt("How many sides does the die have?");
+var numDice = prompt("How many dice?")
+
+function rollDice(numSides) {
+    return Math.floor(Math.random() * numSides) + 1;
+}
+
+for (var i = 0; i < numDice; i++) {
+    console.log(rollDice(numSides));
+}
