76e40ae04f325e597f30dd344ce9115df14755e9
Ahson Qazi <70555455+aqazi328@users.noreply.github.com>
2021-12-09 15:48:30 -0600

    7s

---
+++ luckysevens/backend.js
@@ -0,0 +1,12 @@
+var gameform = document.forms["gameform"];
+var amount = document.getElementById("amount");
+
+function validate(){
+    
+    console.log("validating");
+    if (parseInt(amount.value) <= 0){
+        
+    }
+    return false;
+}
+
